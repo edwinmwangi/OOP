f4669102aee4e6dba3cf0215970f554bd9a1824a
edwinmwangi <edwin.mwangi1@gmail.com>
2016-04-21 02:06:33 +0300

    Added files via upload

---
+++ helloworld2b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=3;
+    while(n<6)
+        {n++;
+            cout<<"Hello World!"<<'\n';
+            
+        }
+return 0;
+}
---
+++ helloworld2c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin>> N;
+
+cout << "Hello , World !\n";
+
+while (--N > 0);
+
+return 0;
+}
