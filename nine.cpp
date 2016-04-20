d8bedab10e2fbb8ab95af6db7979f5690d48b47a
edwinmwangi <edwin.mwangi1@gmail.com>
2016-04-20 20:19:02 +0300

    Added files via upload

---
+++ nine.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i = 0; i <= 10; ++i)
+
+ {
+
+if(i % 2 != 0)
+
+ continue ; // skips all odd numbers
+
+ cout << i << "\n";
+
+ return 0;
+     
+}
+    
+    
+}
