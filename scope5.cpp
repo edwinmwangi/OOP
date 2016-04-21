dc0c41b3faf345131ef738b7b8ff3c35575a5552
edwinmwangi <edwin.mwangi1@gmail.com>
2016-04-21 09:14:03 +0300

    Added files via upload

---
+++ scope5.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<< "Hello,World!\n";// moving the #include <iostream> will allow the code to compile
+    return 0;
+}
---
+++ six.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int x;
+int y;
+int a=10;
+int b=5;
+char result;
+
+    if (a>b)
+        result = 'x';
+    else
+        result = 'y';
+    cout<<result<<'\n';
+    return 0;
+}
